ee8343a43163966ade55603fa79265ca61f121fc
Reham Fattoum <rehamfatoum777@gmail.com>
2024-01-16 23:34:00 +0300

    first commit in weather App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import  WeatherApp from './Component/WeatherApp/weatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      < WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
